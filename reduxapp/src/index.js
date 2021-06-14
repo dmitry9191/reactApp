fbe2d7266e74ff7a06b82f85571c15c5c61bb084
dmitry <dmipushkarjov@yandex.ru>
2021-06-14 21:56:44 +0300

    studying createStore in Redux

--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    
+    default:
+      return 0; 
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer );
+
+console.log(store.getState());
+
+/* let state = reducer(undefined, {});
+
+state = reducer(state, {type: 'INC'});
+console.log(state);
+state = reducer(state, {type: 'INC'});
+console.log(state);
+ */
