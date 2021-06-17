79b7172f1ed4ad112af6495ff9e3e94b8dde3349
Dmitry <dmipu2016@yandex.ru>
2021-06-17 21:39:17 +0300

    created counter

--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -4,17 +4,100 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
-    
+
+    case 'DEC':
+      return state - 1;
+
+    case 'RES':
+      return state = 0;
+
     default:
-      return 0; 
+      return state; 
   }
 }
 
-const store = createStore(reducer );
+const store = createStore(reducer);
 
-console.log(store.getState());
+const inc = () => {
+  return {
+    type: 'INC'
+  }
+}
 
-/* let state = reducer(undefined, {});
+const dec = () => {
+  return {
+    type: 'DEC'
+  }
+}
+
+const res = () => {
+  return {
+    type: 'RES'
+  }
+}
+
+document.querySelector('.btn_item:first-of-type').addEventListener('click', () => {
+  store.dispatch(inc());
+})
+
+document.querySelector('.btn_item:nth-of-type(2)').addEventListener('click', () => {
+  store.dispatch(dec());
+})
+
+document.querySelector('.btn_item:last-of-type').addEventListener('click', () => {
+  store.dispatch(res());
+})
+
+const update = () => {
+  document.querySelector('.number_block').textContent = store.getState();
+}
+
+store.subscribe(() => {
+  update();
+})
+
+/* 
+
+
+
+const rnd = () => {
+  const value = Math.floor(Math.random() * 10);
+
+  return {
+    type: 'RND',
+    value
+  }
+}
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+})
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+  store.dispatch(rnd());
+})
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(() => {
+  update();
+}) */
+
+/* store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+
+let state = reducer(undefined, {});
 
 state = reducer(state, {type: 'INC'});
 console.log(state);
