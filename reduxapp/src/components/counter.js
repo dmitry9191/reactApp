804ea0b7c396df4019731d01586576df80b4d8c8
dmitry <dmipushkarjov@yandex.ru>
2021-06-18 22:15:50 +0300

    refactor to react-redux

---
+++ reduxapp/src/components/counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+/* import {bindActionCreators} from 'redux';*/import './counter.css';
+
+const Counter = ({counter, inc, dec, res}) => {
+    return (
+        <>
+            <div className="number_block">{counter}</div>
+            <div className="btn_block">
+                <div onClick={inc} className="btn_item"></div>
+                <div onClick={dec} className="btn_item"></div>
+                <div onClick={res} className="btn_item"></div>
+            </div>
+        </>  
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    }
+}
+
+/* const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(actions, dispatch);
+} */
+
+export default connect(mapStateToProps, actions)(Counter);
--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -1,106 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-
-    case 'DEC':
-      return state - 1;
-
-    case 'RES':
-      return state = 0;
-
-    default:
-      return state; 
-  }
-}
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
 const store = createStore(reducer);
 
-const inc = () => {
-  return {
-    type: 'INC'
-  }
-}
-
-const dec = () => {
-  return {
-    type: 'DEC'
-  }
-}
-
-const res = () => {
-  return {
-    type: 'RES'
-  }
-}
-
-document.querySelector('.btn_item:first-of-type').addEventListener('click', () => {
-  store.dispatch(inc());
-})
-
-document.querySelector('.btn_item:nth-of-type(2)').addEventListener('click', () => {
-  store.dispatch(dec());
-})
-
-document.querySelector('.btn_item:last-of-type').addEventListener('click', () => {
-  store.dispatch(res());
-})
-
-const update = () => {
-  document.querySelector('.number_block').textContent = store.getState();
-}
-
-store.subscribe(() => {
-  update();
-})
-
-/* 
-
-
-
-const rnd = () => {
-  const value = Math.floor(Math.random() * 10);
-
-  return {
-    type: 'RND',
-    value
-  }
-}
-
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-})
-
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-})
-
-document.getElementById('rnd').addEventListener('click', () => {
-  store.dispatch(rnd());
-})
-
-const update = () => {
-  document.getElementById('counter').textContent = store.getState();
-}
-
-store.subscribe(() => {
-  update();
-}) */
-
-/* store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-
-let state = reducer(undefined, {});
-
-state = reducer(state, {type: 'INC'});
-console.log(state);
-state = reducer(state, {type: 'INC'});
-console.log(state);
- */
+ReactDom.render(<Provider store={store}>
+                  <App/>
+                </Provider>, document.getElementById('root'));
