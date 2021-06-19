3373357cf306b53670086a75fd58c3b92395af05
Dmitry <dmipu2016@yandex.ru>
2021-06-19 22:51:51 +0300

    some little changes

--- reduxapp/src/components/counter.js
+++ reduxapp/src/components/counter.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
-/* import {bindActionCreators} from 'redux';*/import './counter.css';
+/* import {bindActionCreators} from 'redux';*/
+import './counter.css';
 
 const Counter = ({counter, inc, dec, res}) => {
     return (
-        <>
+        <div>
             <div className="number_block">{counter}</div>
             <div className="btn_block">
                 <div onClick={inc} className="btn_item"></div>
                 <div onClick={dec} className="btn_item"></div>
                 <div onClick={res} className="btn_item"></div>
             </div>
-        </>  
+        </div>  
     )
 }
 
--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {createStore} from 'redux';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './components/app';
 
